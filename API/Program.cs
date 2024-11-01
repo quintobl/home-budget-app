a0c234f71a3804b461ab3b53237049244e863c5f
Brad Quinton <bradley.l.quinton@gmail.com>
2024-10-31 22:13:07 -0400

    Add DbContext class and register the service in Program.cs class.

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDbContext<API.Data.DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
