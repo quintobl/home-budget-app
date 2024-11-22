79579e9c04ceaaf8cacafec02fe733fea3d2b470
Brad Quinton <bradley.l.quinton@gmail.com>
2024-11-22 00:04:43 -0500

    Add CommonModule import to app component imports array and ngFor directive to display test data in app component template.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
@@ -5,18 +6,18 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   http = inject(HttpClient);
   title = 'client';
-  users: any;
+  debits: any;
 
   ngOnInit(): void {
     this.http.get('http://localhost:5112/api/debits').subscribe({
-      next: response => this.users = response,
+      next: response => this.debits = response,
       error: error => console.log(error),
       complete: () => console.log('Request has completed')
     })
