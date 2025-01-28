a770b2eb8e81ac5a512c73c1aa946577e51f3aea
Brad Quinton <bradley.l.quinton@gmail.com>
2025-01-27 20:19:38 -0500

    Add account service for purposes of logging in a user. Update navbar template and component to make use of login functionality.

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  private http = inject(HttpClient);
+  baseUrl = 'http://localhost:5112/api/';
+
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'account/login', model);
+  }
+}
