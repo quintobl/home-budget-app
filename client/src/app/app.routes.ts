09ed9793c75c97feec19bb3c3afdc2d73aa62f5a
Brad Quinton <bradley.l.quinton@gmail.com>
2025-01-29 21:26:35 -0500

    Add route guards for each subpage of app.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,12 +4,20 @@ import { DebitsComponent } from './debits/debits.component';
 import { CreditsComponent } from './credits/credits.component';
 import { GraphsChartsComponent } from './graphs-charts/graphs-charts.component';
 import { DataCurrentMonthComponent } from './data-current-month/data-current-month.component';
+import { authGuard } from './_guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'debits', component: DebitsComponent },
-  { path: 'credits', component: CreditsComponent },
-  { path: 'graphs-charts', component: GraphsChartsComponent },
-  { path: 'data-current-month', component: DataCurrentMonthComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [authGuard],
+    children: [
+      { path: 'debits', component: DebitsComponent },
+      { path: 'credits', component: CreditsComponent },
+      { path: 'graphs-charts', component: GraphsChartsComponent },
+      { path: 'data-current-month', component: DataCurrentMonthComponent },
+    ],
+  },
   { path: '**', component: HomeComponent, pathMatch: 'full' },
 ];
